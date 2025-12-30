271df91b2613d267ee2675d6291c73fa8b5f06a0
Kitsune7X <ryuizanagi@proton.me>
2025-12-31 00:47:52 +0900

    complete 7.1

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -11,12 +11,12 @@ const Menu = () => {
       <Link to='/' style={padding}>
         anecdotes
       </Link>
-      <Link to='/create_new' style={padding}>
+      <Link to='/create' style={padding}>
         create new
       </Link>
-      <a href='#' style={padding}>
+      <Link to='/about' style={padding}>
         about
-      </a>
+      </Link>
     </div>
   );
 };
@@ -157,15 +157,12 @@ const App = () => {
       <h1>Software anecdotes</h1>
       <Menu />
 
-      {/* <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer /> */}
-
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route path='/create_new' element={<CreateNew addNew={addNew} />} />
+        <Route path='/create' element={<CreateNew addNew={addNew} />} />
+        <Route path='/about' element={<About />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
