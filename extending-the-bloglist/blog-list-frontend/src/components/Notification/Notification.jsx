1a22a0cfedc989dc74a6fc93995829b314ae4b2d
Kitsune7X <ryuizanagi@proton.me>
2026-01-06 04:02:54 +0900

    write unit test for Notification component

--- extending-the-bloglist/blog-list-frontend/src/components/Notification/Notification.jsx
+++ extending-the-bloglist/blog-list-frontend/src/components/Notification/Notification.jsx
@@ -1,9 +1,12 @@
 import styles from './Notification.module.css';
 
-const Notification = ({ message, isError }) => (
-  <div className={`${styles.notification} ${isError && styles.error}`}>
+const Notification = ({ message, isError = false }) => (
+  <div className={`${styles.notification} ${isError ? styles.error : ''}`}>
     <p>{message}</p>
   </div>
 );
 
 export default Notification;
+
+// TODO: write unit test for `Notification` Component so that after
+// refactoring, it still works
---
+++ extending-the-bloglist/blog-list-frontend/src/components/Notification/Notification.test.jsx
@@ -0,0 +1,33 @@
+import { test, describe, beforeEach, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Notification from './Notification';
+import styles from './Notification.module.css';
+
+describe('Notification component', () => {
+  test('display correct message', async () => {
+    render(<Notification message={'Hello Notification'} />);
+
+    // screen.debug();
+    expect(screen.getByText(/^hello notification$/i)).toBeInTheDocument();
+  });
+
+  test('does not apply error class when there is no error', () => {
+    render(<Notification message={'No error'} />);
+
+    // screen.debug();
+    const div = screen.getByText(/no/i).parentElement;
+
+    expect(div).toHaveClass(styles.notification);
+  });
+
+  test('apply error class when there is error', () => {
+    render(<Notification message={'Oops'} isError={true} />);
+
+    screen.debug();
+
+    const notificationDiv = screen.getByText(/oops/i).parentElement;
+
+    expect(notificationDiv).toHaveClass(styles.error);
+  });
+});
